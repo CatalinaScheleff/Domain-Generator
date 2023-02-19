0e25c9d2483a0fee193da0d0424ff56420599b33
CatalinaScheleff <catascheleff@gmail.com>
2023-02-19 07:37:23 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,44 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  generateDomain();
   console.log("Hello Rigo from the console!");
 };
+
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".net"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        for (let c = 0; c < dom.length; c++) {
+          let nuevo = document.createElement("div");
+          nuevo.innerHTML = pronoun[i] + adj[a] + noun[b] + dom[c];
+          document.querySelector("p").appendChild(nuevo);
+        }
+      }
+    }
+  }
+};
+generateDomain();
+
+// let pronoun = ['the','our'];
+//   let adj = ['great', 'big' ];
+//   let noun = ['jogger','racoon'];
+//   let dom = ['.com', '.net'];
+
+//   for (let i=0;i<pronoun.length;i++) {
+//     for (let a=0;a<adj.lenght;a++) {
+//       for (let b=0;b<noun.lenght;b++) {
+//         for (let c=0;c<dom.lenght;c++) {
+//           let div = document.createElement('div');
+//           div.InnerText = `${pronoun[i]}${adj[a]}${noun[b]}${dom[c]}`
+//           document.body.appendChild(div);
+//         }
+//       }
+//     }
+//   }
